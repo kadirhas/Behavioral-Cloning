b0ae6b3d42a091bf8b64234726b12a9b410e92ea
kadirhas <31936408+kadirhas@users.noreply.github.com>
2017-11-09 12:28:19 +0300

    Initial commit
    Initial commit. Can run on the first track without any problems.

---
+++ clone.py
@@ -0,0 +1,64 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+
+images = []
+measurements = []
+
+correction_factor = 0.2
+correction = [0, correction_factor, correction_factor*-1]
+for line in lines:
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('/')[-1]
+		current_path = 'data/IMG/' + filename
+		image = cv2.imread(current_path)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		images.append(image)
+		measurement = float(line[3])+correction[i]
+		measurements.append(measurement)
+	# source_path = line[0]
+	#filename = source_path.split('/')[-1]
+
+
+
+
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images,measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1.0)
+	
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse',optimizer='adam')
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 1)
+
+model.save('model.h5')
